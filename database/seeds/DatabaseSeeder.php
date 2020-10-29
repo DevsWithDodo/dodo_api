a5569f3297e96484b3617b5f3f8d780af94350fe
Katkó Dominik <katkodominik@gmail.com>
2020-10-29 11:54:27 +0100

    added second group to seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,58 +17,62 @@ class DatabaseSeeder extends Seeder
         $csocsort = factory(App\Group::class)->create([
             'name' => 'Csocsort'
         ]);
+        $other_group = factory(App\Group::class)->create();
         $dominik = factory(App\User::class)->create([
             'username' => 'dominik'
         ]);
         $samu = factory(App\User::class)->create([
             'username' => 'samu'
         ]);
-        $csocsort->members()->attach($dominik->id, [
-            'nickname' => $dominik->username, 'is_admin' => true
-        ]);
-        $csocsort->members()->attach($samu->id, [
-            'nickname' => $samu->username, 'is_admin' => true
-        ]);
 
-        $users = collect([$samu, $dominik])->concat(factory(App\User::class, 5)
-            ->create()
-            ->each(function ($user) use ($csocsort) {
-                $csocsort->members()->attach($user->id, [
-                    'nickname' => $user->username
-                ]);
-            }));
-        $users->each(function ($user) use ($csocsort, $users) {
-            //purchase
-            factory(App\Transactions\Purchase::class, rand(3, 10))
-                ->create([
-                    'group_id' => $csocsort->id,
-                    'buyer_id' => $user->id
-                ])
-                ->each(function ($purchase) use ($csocsort, $users) {
-                    $count = rand(1, 3);
-                    $receivers = $users->random($count);
-                    $receivers->each(function ($receiver) use ($purchase, $count) {
-                        factory(App\Transactions\PurchaseReceiver::class)
-                            ->create([
-                                'purchase_id' => $purchase->id,
-                                'amount' => $purchase->amount / $count,
-                                'receiver_id' => $receiver->id
-                            ]);
+        foreach ([$csocsort, $other_group] as $group) {
+            $group->members()->attach($dominik->id, [
+                'nickname' => $dominik->username, 'is_admin' => true
+            ]);
+            $group->members()->attach($samu->id, [
+                'nickname' => $samu->username, 'is_admin' => true
+            ]);
+
+            $users = collect([$samu, $dominik])->concat(factory(App\User::class, 5)
+                ->create()
+                ->each(function ($user) use ($group) {
+                    $group->members()->attach($user->id, [
+                        'nickname' => $user->username
+                    ]);
+                }));
+            $users->each(function ($user) use ($group, $users) {
+                //purchase
+                factory(App\Transactions\Purchase::class, rand(3, 10))
+                    ->create([
+                        'group_id' => $group->id,
+                        'buyer_id' => $user->id
+                    ])
+                    ->each(function ($purchase) use ($group, $users) {
+                        $count = rand(1, 3);
+                        $receivers = $users->random($count);
+                        $receivers->each(function ($receiver) use ($purchase, $count) {
+                            factory(App\Transactions\PurchaseReceiver::class)
+                                ->create([
+                                    'purchase_id' => $purchase->id,
+                                    'amount' => $purchase->amount / $count,
+                                    'receiver_id' => $receiver->id
+                                ]);
+                        });
                     });
-                });
-            //payment
-            factory(App\Transactions\Payment::class, rand(3, 10))
-                ->create([
-                    'group_id' => $csocsort->id,
-                    'taker_id' => $user->id,
-                    'payer_id' => $users->except($user->id)->random()->id
-                ]);
-            //request
-            factory(App\Request::class, rand(2, 4))
-                ->create([
-                    'group_id' => $csocsort->id,
-                    'requester_id' => $user->id
-                ]);
-        });
+                //payment
+                factory(App\Transactions\Payment::class, rand(3, 10))
+                    ->create([
+                        'group_id' => $group->id,
+                        'taker_id' => $user->id,
+                        'payer_id' => $users->except($user->id)->random()->id
+                    ]);
+                //request
+                factory(App\Request::class, rand(2, 4))
+                    ->create([
+                        'group_id' => $group->id,
+                        'requester_id' => $user->id
+                    ]);
+            });
+        };
     }
 }
