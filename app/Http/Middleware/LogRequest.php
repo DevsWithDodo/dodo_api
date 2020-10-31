7a9cd69f1fcc3a40ee99ac0f89a8fa47629020e6
Katkó Dominik <katkodominik@gmail.com>
2020-10-31 11:27:00 +0100

    logging incoming requests

---
+++ app/Http/Middleware/LogRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Log;
+
+use Closure;
+
+class LogRequest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        Log::info('incoming request', ['request' => $request->all()]);
+        return $next($request);
+    }
+}
