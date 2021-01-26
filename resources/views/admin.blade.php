9678efffc9af553bf340ff9d56144a87e0e216d7
Katkó Dominik <56202545+kdmnk@users.noreply.github.com>
2021-01-26 20:49:57 +0100

    Made the statistics anonymous
    * groups
    
    * fix

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,94 +1,133 @@
-<html lang="en">
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    {{--<link href="https://unpkg.com/tabulator-tables@4.9.1/dist/css/tabulator.min.css" rel="stylesheet">--}}
+    <meta charset="utf-8">
+    <title>Lender - Statistics</title>
+    <link rel="shortcut icon" href="/logo_color.png" type="image/png" />
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('style.css') }}" rel="stylesheet">
     <link href="https://unpkg.com/tabulator-tables@4.9.1/dist/css/materialize/tabulator_materialize.min.css"
         rel="stylesheet">
     <script type="text/javascript" src="https://unpkg.com/tabulator-tables@4.9.1/dist/js/tabulator.min.js"></script>
 </head>
 
 <body>
-    <div style="width:70%;margin-left: auto;margin-right: auto;">
-        <h2>Groups</h2>
-        <div id="grouptable"></div>
-        <h2>Users</h2>
-        <div id="usertable"></div>
-        <form method="POST" action="/admin/send_notification">
-            @csrf
-            <h2>Send notification</h2>
-            to <input id="id" name="id" type="number" min="1" placeholder="id" style="width:40px" />
-            / <input type="checkbox" name="everyone"> everyone
-            <textarea id="message" name="message" placeholder="Message" style="width:100%;margin-top:10;"></textarea>
-            <button type="submit" style="float: right;margin-top:10;">Send</button>
-        </form>
-    </div>
-    <script>
-        //groups
+    <div class="flex-center position-ref">
+        <div class="content">
+            <h1>Groups</h1>
+            <div
+                style="background-color: white; color:black; padding:40px;border-radius: 10px;opacity: 0.9;text-align:left; max-width:80%;margin:0 auto;font-size:15px;font-weight:400;text-align:justify">
+                With one member only: {{$one_member_only}}
+                (do not count in the statistics below)<br>
+                All: {{$all_groups}}<br>
+                Boosted: {{$boosted}}<br>
+                Avarage member number: {{$boosted_members_avg}} (boosted), {{$not_boosted_members_avg}} (not
+                boosted) (guests included)<br>
+                Avarage guests: {{$guests_all_avg}} ({{$guests_avg}} among {{$groups_use_guests}} groups which
+                use them)<br>
+                Avarage purchases: {{$purchases_avg}}<br>
+                Avarage payments: {{$payments_avg}}<br>
+                Avarage purchases: {{$requests_all_avg}} ({{$requests_avg}} among {{$groups_use_requests}} groups which
+                use them)<br>
+                Currencies:
+                @foreach($currencies as $currency => $count)
+                {{$currency}} ({{round($count / $all_groups * 100)}}%) @if (!$loop->last), @endif
+                @endforeach
+                <br><br>
+                Groups with broken balance:
+                <div id="grouptable"></div>
+            </div>
+            <h1>Users</h1>
+            <div
+                style="background-color: white; color:black; padding:40px;border-radius: 10px;opacity: 0.9;text-align:left; max-width:80%;margin:0 auto;font-size:15px;font-weight:400;text-align:justify">
+                Without groups: {{$zero_group}} (do not count in the statistics below)<br>
+                All: {{$all_users}}<br>
+                Guests: {{$guests}}<br>
+
+                Group numbers:
+                @foreach($group_count as $group => $count)
+                {{$group}} ({{round($count)}}),
+                @endforeach
+                avarage group number: {{$group_avg}}<br>
+                Languages:
+                @foreach($languages as $language => $count)
+                {{$language}} ({{round($count / $all_users * 100)}}%) @if (!$loop->last), @endif
+                @endforeach
+            </div>
+            <form method="POST" action="/admin/send_notification">
+                @csrf
+                <h2>Send notification</h2>
+                <div
+                    style="background-color: white; color:black; padding:40px;border-radius: 10px;opacity: 0.9;text-align:left; max-width:80%;margin:0 auto;font-size:15px;font-weight:400;">
+
+                    to <input id="id" name="id" type="number" min="1" placeholder="id" style="width:40px" />
+                    / <input type="checkbox" name="everyone"> everyone
+                    <textarea id="message" name="message" placeholder="Message"
+                        style="width:100%;margin-top:10;"></textarea>
+                    <button type="submit" style="float: right;margin-top:10;">Send</button>
+                </div>
+            </form>
+        </div>
+
+
+        <script>
+            //groups
         var recalculateGroupFormatter = function(cell, formatterParams, onRendered) {
             console.log(cell.getRow().getData()['id']);
             return `<form method='POST' action='/admin/recalculate'>@csrf
                 <input type='hidden' name='group' value=` + cell.getRow().getData()['id'] + `>
                 <input type='submit'></input>`;
         }
-    var groupdata = [
-    @foreach (\App\Group::all() as $group)
+        var groupdata = [
         @php
-        $balance = 0;
-        foreach($group->members as $member){
-            $balance = bcadd($balance, $member->member_data->balance);
-        }
+        $i = 0;
         @endphp
-        {
-            id:{{ $group->id }},
-            @if(config('app.debug'))name:"{{ $group->name}}",@endif
-            created_at:"{{ $group->created_at->format('Y/m/d') }}",
-            members:{{ $group->members->count() }},
-            guests:{{ $group->guests->count()}},
-            member_limit:{{ $group->member_limit ?? 20}},
-            purchases:{{ $group->purchases->count() }},
-            payments:{{ $group->payments->count() }},
-            requests:{{ $group->requests->count() }},
-            balance:{{ $balance }},
-            currency:"{{ $group->currency }}"
-        },
-    @endforeach
-    ];
-    var grouptable = new Tabulator("#grouptable", {
-        data:groupdata,
-        autoColumns:true,
-        autoColumnsDefinitions: function(definitions){
-            definitions.forEach((column) => {
-                if (["members", "guests", "purchases", "payments", "requests"].includes(column.field))
-                    column.bottomCalc = "sum";
-            });
-            return definitions;
-        },
-        layout:"fitDataFill",
-        pagination:"local",
-        paginationSize:10,
+        @foreach (\App\Group::all() as $group)
+            @php
+            $balance = 0;
 
-    });
-    grouptable.addColumn({title:"Recalculate balance",formatter:recalculateGroupFormatter});
+            foreach($group->members as $member){
+                $balance = bcadd($balance, $member->member_data->balance);
+            }
+            @endphp
+            @if($balance != 0 || config('app.debug'))
+            @php
+            $i++;
+            @endphp
+            {
+                id:{{ $group->id }},
+                @if(config('app.debug'))
+                name:"{{ $group->name}}",
+                created_at:"{{ $group->created_at->format('Y/m/d') }}",
+                members:{{ $group->members->count() }},
+                guests:{{ $group->guests->count()}},
+                boosted:{{ $group->boosted}},
+                purchases:{{ $group->purchases->count() }},
+                payments:{{ $group->payments->count() }},
+                requests:{{ $group->requests->count() }},
+                currency:"{{ $group->currency }}",
+                @endif
+                balance:{{ $balance }}
+            },
+            @endif
+        @endforeach
+        ];
+        var grouptable = new Tabulator("#grouptable", {
+            data:groupdata,
+            autoColumns:true,
+            layout:"fitDataFill",
+            pagination:"local",
+            paginationSize:10,
 
-    //users
-    var userdata = [
-    @foreach (\App\User::all() as $user)
-        {id:{{ $user->id }},
-        name:"{{ $user->username }}",
-        registered_at:"{{ $user->created_at->format('Y/m/d') }}",
-        is_guest:{{ $user->is_guest ? 1 : 0 }},
-        groups:{{ $user->groups->count() }},
-        default_currency:"{{ $user->default_currency }}",
-        language:"{{ $user->language }}",
-        },
-    @endforeach
-    ];
-    var usertable = new Tabulator("#usertable", {
-        data:userdata,
-        autoColumns:true,
-        layout:"fitDataFill",
-        pagination:"local",
-        paginationSize:10,
-    });
-    </script>
+        });
+        @if($i)
+        grouptable.addColumn({title:"Recalculate balance",formatter:recalculateGroupFormatter});
+        @endif
+        </script>
+    </div>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Http\Request;
 use App\Notifications\CustomNotification;
 
 use App\Group;
+use App\Http\Controllers\CurrencyController;
+use App\User;
+use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,7 +33,98 @@ Route::get('/join/{token}', function ($token) {
 });
 
 Route::get('/admin', function () {
-    return view('admin');
+    $not_boosted_members = $boosted_members = 0;
+    $not_boosted_guests = $boosted_guests = 0;
+    $boosted = $not_boosted = $all = 0;
+    $payments = $purchases = 0;
+    $groups_use_requests = $requests_all = $requests_use = 0;
+    $groups_use_guests = $guests_all = $guests_use = 0;
+    $one_member_only = Group::has('members', '=', 1)->count();
+    $groups = Group::has('members', '>=', 2)->get();
+    $currencies = [];
+    foreach ($groups as $group) {
+        if ($group->boosted) {
+            $boosted_members += $group->members()->count();
+            $boosted++;
+        } else {
+            $not_boosted_members += $group->members()->count();
+            $not_boosted++;
+        }
+        $all++;
+        $payments += $group->payments()->count();
+        $purchases += $group->purchases()->count();
+        $requests= $group->requests()->count();
+        $requests_all += $requests;
+        if ($requests) {
+            $requests_use += $requests;
+            $groups_use_requests++;
+        }
+        $guests = $group->guests()->count();
+        $guests_all += $guests;
+        if ($guests){
+            $guests_use += $guests;
+            $groups_use_guests++;
+        }
+        if (isset($currencies[$group->currency])){
+            $currencies[$group->currency]++;
+        }else {
+            $currencies[$group->currency] = 1;
+        }
+    }
+    asort($currencies);
+
+    $zero_group = User::has('groups', '=', 0)->where('password', '<>', null)->count();
+    $users = User::has('groups', '>=', 1)->where('password', '<>', null)->with('groups')->get();
+    $all_users = $users->count();
+    $guests = User::where('password', null)->count();
+    $groups = 0;
+    $languages = [];
+    $group_count = [];
+    foreach ($users as $user) {
+        $groups += $user->groups()->count();
+
+        if (isset($group_count[$user->groups()->count()])){
+            $group_count[$user->groups()->count()]++;
+        }else {
+            $group_count[$user->groups()->count()] = 1;
+        }
+
+        if (isset($languages[$user->language])){
+            $languages[$user->language]++;
+        }else {
+            $languages[$user->language] = 1;
+        }
+    }
+    asort($languages);
+    arsort($group_count);
+    return view('admin', [
+        'not_boosted_members' => $not_boosted_members,
+        'one_member_only' => $one_member_only, 2,
+        'boosted' => $boosted,
+        'not_boosted' => $not_boosted,
+        'all_groups' => $all,
+        'boosted_members_avg' => round($boosted_members / $boosted, 2),
+        'not_boosted_members_avg' => round($not_boosted_members / $not_boosted, 2),
+        'boosted_guests_avg' => round($boosted_guests / $boosted, 2),
+        'not_boosted_guests_avg' => round($not_boosted_guests / $not_boosted, 2),
+        'payments_avg' => round($payments / $all, 2),
+        'purchases_avg' => round($purchases / $all, 2),
+        'requests_all_avg' => round($requests_all / $all, 2),
+        'requests_avg' => round($requests_use / ($groups_use_requests ? $groups_use_requests : 1), 2),
+        'groups_use_requests' => $groups_use_requests,
+        'guests_all_avg' => round($guests_all / $all, 2),
+        'guests_avg' => round($guests_use / ($groups_use_guests ? $groups_use_guests : 1), 2),
+        'groups_use_guests' => $groups_use_guests,
+        'currencies' => $currencies,
+        //users
+        'zero_group' => $zero_group,
+        'group_count' => $group_count,
+        'all_users' => $all_users,
+        'guests' => $guests,
+        'group_avg' => round($groups / $all_users, 2),
+        'languages' => $languages
+    ]);
+
 })->middleware('passwordprotect:1');
 
 Route::post('/admin/recalculate', function (Request $request) {
