4fe0d29629f5fdf648d2fdfcf2b261d212965f74
katkodominik <katkodominik@gmail.com>
2022-12-24 23:43:41 +0100

    fix purchase resource

--- app/Http/Resources/Purchase.php
+++ app/Http/Resources/Purchase.php
@@ -26,6 +26,7 @@ class Purchase extends JsonResource
             'buyer_nickname' => Group::nicknameOf($this->group_id, $this->buyer_id),
             'total_amount' => round(floatval($this->amount), 2),
             'original_total_amount' => round(floatval($this->original_amount), 2),
+            'original_currency' => $this->original_currency,
             'receivers' => PurchaseReceiver::collection($this->receivers),
             'reactions' => Reaction::collection($this->reactions)
         ];
