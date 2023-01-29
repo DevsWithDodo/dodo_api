30c4f813a7c0de06564629e12d6cc1a836c4f16b
katkodominik <katkodominik@gmail.com>
2023-01-29 23:08:33 +0100

    revert dodoapp://dodoapp

--- resources/views/join.blade.php
+++ resources/views/join.blade.php
@@ -37,7 +37,7 @@
         @if($group != null)
         window.onload = function() {
             //Deep link URL for users with app already installed on their device
-            window.location = 'dodoapp://dodoapp/join/{{ $group->invitation }}';
+            window.location = 'lenderapp://lenderapp/join/{{ $group->invitation }}';
         }
         @endif
 
@@ -68,7 +68,7 @@
             </div>
         </div>
         @if($group != null)
-            <a class="joinBtn" id="joining" href="dodoapp://dodoapp/join/{{ $group->invitation }}" target="_blank">
+            <a class="joinBtn" id="joining" href="lenderapp://lenderapp/join/{{ $group->invitation }}" target="_blank">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="38px"><path d="M11,7L9.6,8.4l2.6,2.6H2v2h10.2l-2.6,2.6L11,17l5-5L11,7z M20,19h-8v2h8c1.1,0,2-0.9,2-2V5c0-1.1-0.9-2-2-2h-8v2h8V19z"/></svg>
                 <div style="display: flex; flex-direction:column; justify-content: center;">
                     <span class="join_group">Join group</span>
